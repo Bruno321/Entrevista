f5bafde935176d66ca672f354af534e26deacd76
Bruno <brunopaz00@yahoo.com.mx>
2022-07-21 23:19:36 -0500

    primer commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainPage from './pages/MainPage';
+
+export const App = () => {
+ 
+    return(
+        <>
+            <MainPage />
+        </>
+    )
+    
+}
+
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// Styles
+import './Header.css'
+
+// Assets
+import title from '../../assets/img/title.png'
+import twitter from '../../assets/img/twitter.png'
+import discord from '../../assets/img/discord.png'
+import instagram from '../../assets/img/instagram.png'
+import email from '../../assets/img/email.png'
+
+export const Header = () => {
+ 
+    return(
+        <div className='header'>
+          <div className='title-container'>
+            <img src={title}></img>
+          </div>
+          <div className='social-media-container'>
+            <img src={twitter}></img>
+            <img src={discord}></img>
+            <img src={instagram}></img>
+            <img src={email}></img>
+          </div>
+        </div>
+    )
+    
+}
+
+export default Header
